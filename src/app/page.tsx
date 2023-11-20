cfbf4b72dc949ea9724893fdda77704e912aefa0
JayCode Developer <JayCode.contakt@gmail.com>
2023-11-20 12:24:59 +0100

    Create List component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Form from "@/components/form"
+import List from "@/components/list"
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
       <Form />
+      <List/>
     </main>
   )
 }
