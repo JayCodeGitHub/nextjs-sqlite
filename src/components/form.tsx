6ae0b88b919e86179a6941f66d46565b94d783ed
JayCode Developer <JayCode.contakt@gmail.com>
2023-11-20 09:32:22 +0100

    Add inputs to form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 export default function Form() {
+
+    const [form, setForm] = useState(null);
+
+
     return (
       <form>
-        <h1>form</h1>
+         <input
+            className="w-full"
+            placeholder="Title"
+            type="text"
+            name="title"
+            required
+          />
+          <textarea
+            className="w-full"
+            placeholder="Write something interesting..."
+            name="content"
+            required
+          />
+           <button
+            type="submit"
+            className="font-medium bg-sky-500 text-white px-3 py-1"
+          >
+            Create post
+          </button>
       </form>
     )
 }
