6724801d4914c453807137b8fef20d55ccb712d7
JayCode Developer <JayCode.contakt@gmail.com>
2023-11-20 09:58:10 +0100

    Add form validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <Form/>
+      <Form />
     </main>
   )
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,32 +1,65 @@
+"use client"
 import { useState } from "react";
 
+
 export default function Form() {
+    const [error, setError] = useState<null | string>(null);
+    const [form, setForm] = useState({
+        title: '',
+        content: '',
+    });
 
-    const [form, setForm] = useState(null);
+    const validate = (form: any) => {
+        if(!form.title) {
+            return "Title is required"
+        }
 
+        if(!form.content) {
+            return "Content is required"
+        }
+    }
+
+    const updateField = (e: any) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        const error = validate(form)
+        if(error) {
+            setError(error);
+            console.log(error);
+            return
+        }
+        console.log(form)
+    }
 
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
          <input
             className="w-full"
             placeholder="Title"
             type="text"
             name="title"
+            onChange={updateField}
             required
           />
           <textarea
             className="w-full"
             placeholder="Write something interesting..."
             name="content"
+            onChange={updateField}
             required
           />
            <button
             type="submit"
-            className="font-medium bg-sky-500 text-white px-3 py-1"
+            className="font-medium  px-3 py-1"
           >
             Create post
           </button>
       </form>
     )
 }
-  
